d1849ca8843f4c5ff8089d878feadf21c745f775
Manisha <18manisha2019@gmail.com>
2020-08-25 11:56:11 +0530

    Update ProductDao.java
    Added clear comment

--- demoJpa/src/main/java/com/example/demoJpa/dao/ProductDao.java
+++ demoJpa/src/main/java/com/example/demoJpa/dao/ProductDao.java
@@ -3,7 +3,7 @@ package com.example.demoJpa.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demoJpa.entity.ProductEntity;
-//no need to write code as Springboot will create classes at bg
+//no need to write code as Springboot will create classes at background
 public interface ProductDao extends JpaRepository<ProductEntity, Integer>{
 
 }
